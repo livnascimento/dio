eb138a09591bb8bd6a654bd4641c0b9ea129b4d3
Lívia Nascimento <liviapnd@gmail.com>
2023-11-20 13:03:50 -0300

    feat: card resultado

--- desenvolvimento-frontend-com-angular-banco-pan/explorando-angular-framework/bugzfeed/src/app/components/card-result/card-result.component.ts
+++ desenvolvimento-frontend-com-angular-banco-pan/explorando-angular-framework/bugzfeed/src/app/components/card-result/card-result.component.ts
@@ -12,10 +12,10 @@ export class CardResultComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  title: string = "Dev Tainá - Uma Aventura na Amazônia";
-  subtitle: string = "Quem mexeu na prantinha da minha mesa? 🌱";
-  imgSrc: string = "https://pm1.aminoapps.com/6723/12fd6a9e0764b2d9e9dd95bff3117c272ad160fdv2_hq.jpg";
-  description: string = "Você ama a natureza, super good vibes e passa longe do café porque faz mal pra ansiedade. Acredita que o front-end tem que estar bonito e o back-end tem que consumir o mínimo possível de recursos.";
+  title: string = "";
+  subtitle: string = "";
+  imgSrc: string = "";
+  description: string = "";
 
   result: boolean = true;
 }
