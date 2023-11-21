ca2eb3957939133d50d3ee365c317e6e93889ef7
Lívia Nascimento <liviapnd@gmail.com>
2023-11-21 19:12:46 -0300

    feat: sessão veja também

---
+++ desenvolvimento-frontend-com-angular-banco-pan/explorando-angular-framework/bugzfeed/src/app/components/see-more-section/see-more-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-see-more-section',
+  templateUrl: './see-more-section.component.html',
+  styleUrls: ['./see-more-section.component.css']
+})
+export class SeeMoreSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
