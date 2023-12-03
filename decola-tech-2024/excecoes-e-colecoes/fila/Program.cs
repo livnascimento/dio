61c1ea87c897a660f71aee35cb178a01e49e32d6
Lívia Raiane <liviaraianen@gmail.com>
2023-12-02 21:30:41 -0300

    feat: código da aula de sobre filas

---
+++ decola-tech-2024/excecoes-e-colecoes/fila/Program.cs
@@ -0,0 +1,12 @@
+﻿var nomes = new Queue<string>();
+
+nomes.Enqueue("Lívia");
+nomes.Enqueue("Lucas");
+nomes.Enqueue("Ana");
+
+nomes.Dequeue();
+
+foreach (string nome in nomes)
+{
+    Console.WriteLine(nome);
+}
