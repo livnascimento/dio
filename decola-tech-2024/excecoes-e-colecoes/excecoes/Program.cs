5786c4f5b8ac6c7b1a9531c847755df1295eba35
Lívia Raiane <liviaraianen@gmail.com>
2023-12-02 21:31:11 -0300

    feat: código da aula de sobre exceções, try/catch/finally

---
+++ decola-tech-2024/excecoes-e-colecoes/excecoes/Program.cs
@@ -0,0 +1,20 @@
+﻿using excecoes_e_colecoes.Models;
+using System.Globalization;
+
+try
+{
+	string[] lines = File.ReadAllLines("Files/alunos.txt");
+
+	foreach (string line in lines)
+	{
+		Console.WriteLine(line);    
+	}	
+}
+catch (FileNotFoundException ex)
+{
+	Console.WriteLine($"O seguinte erro ocorreu: {ex.Message}");
+}
+catch (Exception ex)
+{
+	Console.WriteLine($"O seguinte erro ocorreu: {ex.Message}");
+}
