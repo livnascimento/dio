1f72821e90176287236e975a448e45c7190cc5db
Lívia Raiane <liviaraianen@gmail.com>
2023-12-09 10:55:05 -0300

    feat: código da aula de tuplas

---
+++ formacao-dotnet-developer/tuplas-ternario-desconstrucao/Program.cs
@@ -0,0 +1,10 @@
+﻿using formacao.Models;
+
+var (sucesso, linhas, quantidadeLinhas) = LeituraArquivo.LerArquivo("arquivo.txt");
+string mensagem = sucesso ? "deu certo" : "não deu certo";
+
+Console.WriteLine($"A leitura do arquivo {mensagem}! {quantidadeLinhas} foram lidas.");
+foreach (string linha in linhas)
+{
+    Console.WriteLine(linha);
+}
