7eea67ca0e93db1cbc4917571eb2b477af4a439a
Lívia Nascimento <liviaraianen@gmail.com>
2023-12-15 17:17:51 -0300

    feat: contexto da api

--- formacao-dotnet-developer/modulo-api/EntityFramework/Context/AgendaContext.cs
+++ formacao-dotnet-developer/modulo-api/EntityFramework/Context/AgendaContext.cs
@@ -2,18 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using EntityFramework.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace EntityFramework.Context
 {
     public class AgendaContext : DbContext
     {
-        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        public AgendaContext(DbContextOptions options) : base(options)
         {
-
         }
-
-        public DbSet<Contato> Contatos {get; set;}
+        public DbSet<Contato> Contatos { get; set; }
     }
 }
