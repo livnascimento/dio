b72e2d6545f1f5929c6ec74bc3d4bb6b05f11195
Lívia Nascimento <liviaraianen@gmail.com>
2023-12-23 18:42:20 -0300

    feat: criar e editar contato

--- formacao-dotnet-developer/modulo-api/mvc/Controllers/ContatoController.cs
+++ formacao-dotnet-developer/modulo-api/mvc/Controllers/ContatoController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using mvc.Context;
+using mvc.Models;
 
 namespace mvc.Controllers
 {
@@ -21,6 +22,45 @@ namespace mvc.Controllers
         {
             var contatos = _context.Contatos.ToList();
             return View(contatos);
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Criar(Contato contato)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Contatos.Add(contato);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contato);
+        }
+        public IActionResult Editar(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+            return View(contato);
+        }
+        [HttpPost]
+        public IActionResult Editar(Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(contato.Id);
+
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
